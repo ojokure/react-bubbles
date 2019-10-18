2701817098121fe764a857f5e989febf495083bf
oladimeji-ojo <ojokuredim@gmail.com>
2019-10-18 08:39:04 +0100

    created a login page and an axios instance auxwithAuth

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,73 @@
 import React from "react";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+// make a post request to retrieve a token from the api
+// when you have handled the token, navigate to the BubblePage route
 
-export default Login;
+export default function Login(props) {
+  const credentials = {
+    username: "",
+    password: ""
+  };
+
+  const [state, setState] = useState(credentials);
+
+  const handleChange = e => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const postCredentials = {
+      username: state.username,
+      password: state.password
+    };
+
+    axiosWithAuth()
+      .post("/login", postCredentials)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/friends");
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      .finally(Response => {
+        console.log(Response);
+      });
+  };
+
+  return (
+    <div>
+      <div className="App">
+        <h1> Please Sign In </h1>
+
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Username </label>
+            <br />
+            <input
+              type="text"
+              name="username"
+              onChange={handleChange}
+              value={state.username}
+            />
+          </div>
+          <br />
+          <div>
+            <label>Password </label>
+            <br />
+            <input
+              type="text"
+              name="password"
+              onChange={handleChange}
+              value={state.password}
+            />
+          </div>
+
+          <br />
+          <button type="submit">Submit </button>
+        </form>
+      </div>
+    </div>
+  );
+}
